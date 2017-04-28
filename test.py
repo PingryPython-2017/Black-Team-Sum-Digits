0da8fdc1af9189951f25a73dc0f80e19a713850c
Jonathan Chen <jchen2019@pingry.org>
2017-04-28 14:08:48 -0400

    added a few tests to test the function

---
+++ test.py
@@ -0,0 +1,6 @@
+import sumDigits
+
+print("The Sum of the Digits in the number 25 is: " + str(sumDigits.sum_digits(25)))
+print("The Sum of the Digits in the number 0 is: " + str(sumDigits.sum_digits(0)))
+print("The Sum of the Digits in the number 1 is: " + str(sumDigits.sum_digits(1)))
+print("The Sum of the Digits in the number 5738 is: " + str(sumDigits.sum_digits(5738)))
