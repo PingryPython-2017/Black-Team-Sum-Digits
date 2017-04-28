89d3f4ad7632c77f7c93dcd71c0a2cb7a78e69be
Samantha Burak <sburak2020@pingry.org>
2017-04-28 14:26:18 -0400

    Wrote main program

---
+++ main.py
@@ -0,0 +1,7 @@
+import sumDigits
+
+num = input("\nEnter a number: ")
+num = int(num)
+
+print("The sum of the digits of that number is {}".format(sumDigits.sum_digits(num)))
+print("Thanks for using this program. Have a janky day.\n")
