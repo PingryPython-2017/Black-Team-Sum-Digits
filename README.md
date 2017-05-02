31c3e112f919c97b954581109bb2658b12a415f0
Ian Larson <ilarson2020@pingry.org>
2017-05-02 12:30:15 -0400

    Added a README, and finished our function.,..........:).

---
+++ README.md
@@ -0,0 +1,16 @@
+SUM DIGITS
+==========
+Takes in a number, and adds up its digits.
+
+Author
+------
+Black Team
+
+License
+-------
+unlicense.org
+
+Date
+-------
+5/2/17
+
--- sumDigits.py
+++ sumDigits.py
@@ -1,8 +1,9 @@
 # sum_digits
 
 def sum_digits(num):
-	'''Takes in a nonnegative integer, and tells the sum of each of the digits.'''
-	if num == 0:
-		return 0
+	'''Takes in a non-negative integer, and tells the sum of each of the digits.'''
+	if num == 0: # Making a terminating case, with an if statement
+		return 0 # Terminating Case always returns 0
 	else:
-		return num % 10 + sum_digits(num // 10)
+		return num % 10 + sum_digits(num // 10) # Recursive Case
+
